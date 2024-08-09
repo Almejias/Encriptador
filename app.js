a4aba2374e52f5e2ad71089bc0ad4e46384e2b6a
Almejias <almejiasart@gmail.com>
2024-08-09 16:29:55 -0400

    Añadiendo boton de copiar y variables css

--- app.js
+++ app.js
@@ -6,21 +6,26 @@ function encriptar() {
     document.getElementById("imagen1").style.display = "none";
     //Logica de encriptacion
     let textoUsuario = document.getElementById('valorUsuario').value;
-    console.log(textoUsuario)
+    console.log(textoUsuario);
     //proximamente cambiar variable cuando se tenga la logica
     let textoEncriptado = textoUsuario.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
     //Para cambiar el textoProcesado a el texto encriptado
     document.getElementById("textoProcesado").innerHTML = textoEncriptado;
+    //Boton copiar
 }
 
 
 function desencriptar() {
     //Tomando el texto encriptado del usuario
     let textoUsuarioDensencriptado = document.getElementById('valorUsuario').value;
-    console.log(textoUsuarioDensencriptado)
+    console.log(textoUsuarioDensencriptado);
     //Desencriptando texto
     let textoDesencriptado = textoUsuarioDensencriptado.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
     //Desencriptando texto
     document.getElementById("textoProcesado").innerHTML = textoDesencriptado;
 }
 
+function copiar() {
+    let textoProcesado = document.getElementById('textoProcesado').innerHTML;
+        navigator.clipboard.writeText(textoProcesado)
+}
